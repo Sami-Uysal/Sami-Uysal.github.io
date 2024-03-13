da6cc0ae187a34a802de819a1c94976d440e51fa
Sami Uysal <samiuysal.business@gmail.com>
2024-03-13 17:44:11 +0300

    samiuysal.com's codes have been transferred.

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 mb-4">
+                <div class="profile">
+                    <img src="profile-avatar.png" alt="Profile Picture" class="img-fluid">
+                    <h1 class="mb-4">Sami Uysal</h1>
+                    <p class="mb-4"><i>I'm a young person who tries all sorts of ways to achieve his dreams and deals with software.</i></p>
+                </div>
+                <div class="social d-flex justify-content-">
+                    <a href="https://github.com/Sami-Uysal" target="_blank"><i class="fab fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/samiuysal/" target="_blank"><i class="fab fa-linkedin"></i></a>
+                    <a href="mailto:samiuysal.business@gmail.com" target="_blank"><i class="fas fa-envelope"></i></a>
+                </div>
+            </div>
+            <div class="col-md-6 col-md-6 d-flex flex-column align-items-center justify-content-center">
+                <a href="https://tryhackme.com/p/Narheur" target="_blank">
+                    <div class="box mb-3">
+                        <div class="box-left">
+                            <span>Tryhackme</span>
+                        </div>
+                        <div class="box-right"> 
+                            <i class="fas fa-arrow-right"></i>
+                        </div>
+                    </div>
+                </a>
+                <a href="https://g.dev/samiuysal" target="_blank">
+                    <div class="box mb-3">    
+                        <div class="box-left">
+                            <span>Google Developers</span>
+                        </div>
+                        <div class="box-right"> 
+                            <i class="fas fa-arrow-right"></i>
+                        </div>
+                    </div>
+                </a>
+                <a href="https://www.credly.com/users/sami-uysal/badges" target="_blank">
+                    <div class="box mb-3">
+                        <div class="box-left">
+                            <span>Credly</span>
+                        </div>
+                        <div class="box-right"> 
+                            <i class="fas fa-arrow-right"></i>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            document.body.classList.add('loaded');
+        });
+    </script>
+</body>
+
+</html>
